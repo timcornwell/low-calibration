e98f155cc4f3f4bb4a9c5b0d42b7345495682b22
timcornwell <realtimcornwell@gmail.com>
2016-07-26 15:31:05 +0100

    n,m reversed

--- testzernike.py
+++ testzernike.py
@@ -14,7 +14,7 @@ print("Working from Git repository %s" %
 plt.clf()
 r=numpy.arange(0.9, 1.0, 0.0001)
 for j in range(1,10000,1000):
-    m, n = noll_to_nm(j+1)
+    n, m = noll_to_nm(j+1)
     z=zernike(m, n, rho=r, phi=numpy.pi/4.0)
     plt.plot(r, z, '-')
 plt.show()
