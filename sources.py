Correct number of sources calculation

sources.py
@@ -7,14 +7,16 @@ from scipy import interpolate
 from scipy.constants import k
 
 class sources:
-# Randomly chosen strength.
+# The number counts are a CDF when normalised correctly.
     def randomsources(self, smin, freq, FOV):
-        nsources=int(self.numbers(smin, freq))
+        nsources=int(FOV*self.numbers(smin, freq))
+        cdf = lambda x: (FOV*self.numbers(x, freq)/float(nsources))
         S=numpy.ones(nsources)
         for nsource in range(nsources):
             p=random.uniform(0.0, 1.0)
-            for flux in numpy.arange(smin, 100*smin, 0.01*smin):
-                if (p * nsources > self.numbers(flux, freq)*FOV):
+            for expflux in numpy.arange(numpy.log10(smin), 1.0, 0.01):
+                flux=10**expflux
+                if (p > cdf(flux)):
                     S[nsource] = flux
                     break
         return sorted(S)